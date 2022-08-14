fbee6aad18ffc81852d7be2f7514c47b7c037f20
Tiago Gomes <tiagogomes187@gmail.com>
2022-08-13 21:36:45 -0300

    Generics

---
+++ 16-Generics-Set-Map/java-oo-16/src/course/application/Program.java
@@ -0,0 +1,28 @@
+package course.application;
+
+import course.services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //PrintService<Integer> ps = new PrintService<>();
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        String x = ps.first();
+        System.out.println("First: " + x);
+
+        sc.close();
+    }
+}
